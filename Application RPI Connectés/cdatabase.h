ca9d3fa5af1d19a97d434bdd5c8ad098ccdfd82e
VolgarIII <jujurich037@gmail.com>
2024-03-28 15:45:40 +0100

    Ajout Application RPI Connectés

---
+++ Application RPI Connectés/cdatabase.h
@@ -0,0 +1,27 @@
+#ifndef CDATABASE_H
+#define CDATABASE_H
+
+
+#include <QString>
+#include <QSqlDatabase>
+#include <filesystem>
+using namespace std;
+
+class cdatabase
+{
+    QSqlDatabase db;
+    QString ip;
+    filesystem::path cheminDossierSauvegarde = filesystem::current_path()/ "dossierREM";
+
+public:
+    cdatabase(QString IP, QString Db, QString username, QString password, QString Port);
+    ~cdatabase();
+    QStringList getListeRPIConnectes();
+    bool exporterDatabaseSql();
+    bool importerDatabaseSql();
+
+
+
+};
+
+#endif // CDATABASE_H
